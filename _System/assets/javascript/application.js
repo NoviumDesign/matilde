d92618f30c8144e0d9f476548ea6b9b8ee5c5cec
Samuel Lie <samuel.lie1@gmail.com>
2016-10-13 11:34:41 +0200

    Index page

---
+++ _System/assets/javascript/application.js
@@ -0,0 +1,23 @@
+var navToggle   = document.getElementById('do-toggle-nav'),
+    body        = document.body,
+    menuItems   = document.getElementById('main-nav').getElementsByTagName('a');
+
+navToggle.addEventListener('click', function() {
+  toggleNav();
+}, false);
+
+for (var i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('click', function() {
+    toggleNav();
+  }, false);
+}
+
+function toggleNav() {
+  if (body.classList.contains('nav-open')) {
+    body.classList.remove('nav-open');
+    navToggle.textContent = 'Visa meny';
+  } else {
+    body.classList.add('nav-open');
+    navToggle.textContent = 'Stäng meny';
+  }
+}
---
+++ _System/public/templates/index-news.tpl
@@ -0,0 +1,5 @@
+<!-- Displays latest news post -->
+<h3 class="post-title"><a href="{{ url }}">{{ title }}</a></h3>
+<p>
+  {{ body | striphtml }}
+</p>
